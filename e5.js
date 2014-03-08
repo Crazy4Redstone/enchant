d33b85dbdd105c8fd587817fb75072ed8d228550
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-08 08:56:00 -0600

    Create e5.js

---
+++ e5.js
@@ -0,0 +1,23 @@
+// Made by Crazy4Redstone
+
+// Functions
+
+
+
+
+
+
+// Blocks
+
+Block.defineBlock (116,'Enchantment Table',[['enchanting_table_bottom',0],['enchanting_table_top',0],['enchanting_table_side',0],['enchanting_table_side',0],['enchanting_table_side',0],['enchanting_table_side',0]])
+Block.setShape (116,0,0,0,1,3/4,1)
+Block.setDestroyTime (116,0.01)
+Block.setRenderLayer (116,1)
+
+function destroyBlock (x,y,z,id)
+{
+	if (id == 116)
+	{
+	Level.dropItem (x,y,z,116,0,1)
+	}
+}
