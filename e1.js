a633af85e72c3b834d3ca22545f8ba688b21e37f
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-08 00:49:31 -0600

    Create e1.js

---
+++ e1.js
@@ -0,0 +1,13 @@
+// Made by Crazy4Redstone
+
+Block.defineBlock (116,”Enchantment Table”,[[“enchanting_table_bottom”,0,”enchanting_table_top”,0,”enchanting_table_side”,0,”enchanting_table_side”,0,”enchanting_table_side”,0,”enchanting_table_side”,0]],1,false,0)
+Block.setShape (116,0,0,0,1,3/4,1)
+Block.setDestroyTime (116,0.01)
+
+function destroyBlock (x,y,z,id)
+{
+	if (id == 116)
+	{
+	Level.dropItem (x,y,z,0,116,1)
+	}
+}
