558dd81a476cb11e3da3dbedd34ca22a5f4df56d
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-08 09:02:39 -0600

    Create e6.js

---
+++ e6.js
@@ -0,0 +1,18 @@
+// Made by Crazy4Redstone
+
+// Functions
+
+
+
+
+
+
+// Blocks
+
+Block.defineBlock (116,'Enchantment Table',[['enchanting_table_bottom',0],['enchanting_table_top',0],['enchanting_table_side',0],['enchanting_table_side',0],['enchanting_table_side',0],['enchanting_table_side',0]])
+Block.setShape (116,0,0,0,1,3/4,1)
+Block.setDestroyTime (116,0.01)
+Block.setRenderLayer (116,1)
+
+ModPE.addCraftRecipe(116, 1, 0, [1,0]);
+/*[340, 0, 264, 0, 264, 0, 49, 0, 49, 0, 49, 0, 49, 0]); //crafting recipe: 1 book, 2 diamonds, 4 obsidian*/
