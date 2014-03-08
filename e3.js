91d82e3984ec8d863abdd8fb520c984e4aad55b4
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-08 01:10:06 -0600

    Update e3.js

--- e3.js
+++ e3.js
@@ -1,6 +1,6 @@
 // Made by Crazy4Redstone
 
-Block.defineBlock (116,'Enchantment_Table',[['enchanting_table_bottom',0,'enchanting_table_top',0,'enchanting_table_side',0,'enchanting_table_side',0,'enchanting_table_side',0,'enchanting_table_side',0]],1,false,7)
+Block.defineBlock (116,'Enchantment_Table',[['enchanting_table_bottom',0]],[['enchanting_table_top',0]],[['enchanting_table_side',0]],[['enchanting_table_side',0]],[['enchanting_table_side',0]],[['enchanting_table_side',0]],1,false,7)
 Block.setShape (116,0,0,0,1,3/4,1)
 Block.setDestroyTime (116,0.01)
 Block.setRenderLayer (116,2)
