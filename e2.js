af0b2ffcc068f7e5ff95e826209ed5b0b87ca5e9
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-08 01:02:04 -0600

    Update e2.js

--- e2.js
+++ e2.js
@@ -1,6 +1,6 @@
 // Made by Crazy4Redstone
 
-Block.defineBlock (116,”Enchantment_Table”,[[“enchanting_table_top”,0]],1,false,0)
+Block.defineBlock (116,'Enchantment_Table',[['enchanting_table_top',0]],1,false,0)
 Block.setShape (116,0,0,0,1,3/4,1)
 Block.setDestroyTime (116,0.01)
 
